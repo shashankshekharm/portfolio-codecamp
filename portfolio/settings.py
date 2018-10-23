289a540b8f9e6accff3779a9956ffa80b145ca2a
shashank shekhar <shekharshashank922@gmail.com>
2018-10-23 11:42:02 +0530

    final commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -121,8 +121,14 @@ USE_TZ = True
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'portfolio/static/')
 ]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
